9df1789b834687f2a6c7f0424d6ef8cd707c8583
Falcon-JasonM <87274520+Falcon-JasonM@users.noreply.github.com>
2023-12-09 00:38:59 -0600

    "added about section to blog"

--- blog.js
+++ blog.js
@@ -20,12 +20,41 @@ function postImage(imageUrl) {
 }
 
 // Function to update the blog page
+
 function updateBlogPage() {
     // Code to update the blog page with the new blog posts
     // For example, you can manipulate the DOM to add the new posts to the HTML
+
+    // Get the container element where the blog posts will be added
+    const blogContainer = document.getElementById("blogContainer");
+
+    // Create a new blog post element
+    const newBlogPost = document.createElement("div");
+    newBlogPost.classList.add("blog-post");
+
+    // Get the title and content of the new blog post
+    const title = blogPosts[blogPosts.length - 1].title;
+    const content = blogPosts[blogPosts.length - 1].content;
+
+    // Create the title element
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = title;
+
+    // Create the content element
+    const contentElement = document.createElement("p");
+    contentElement.textContent = content;
+
+    // Append the title and content elements to the new blog post element
+    newBlogPost.appendChild(titleElement);
+    newBlogPost.appendChild(contentElement);
+
+    // Append the new blog post element to the blog container
+    blogContainer.appendChild(newBlogPost);
+
     // Here, we are just logging the update for demonstration purposes
     console.log("Blog page updated");
 }
+
 
 // Function to handle the click event of the "Add Blog Post" button
 function handleAddBlogPostClick() {
